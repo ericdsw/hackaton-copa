c6b06015717356a02cb0f1c0ef3e8d39a44da2aa
Eric De Sedas <eric.desedas.w@gmail.com>
2019-06-01 11:28:52 -0500

    Create UI Skeleton (#7)
    * Create UI skeleton
    * Complete base ApplicationWrapper

---
+++ ui/src/App.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import ApplicationWrapper from './components/commons/ApplicationWrapper';
+import { MuiThemeProvider, createMuiTheme, Theme } from '@material-ui/core/styles';
+import { CssBaseline, withStyles, createStyles } from '@material-ui/core';
+import { purple, green } from '@material-ui/core/colors';
+
+const applicationTheme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green
+  }
+});
+
+const styles = ({mixins, breakpoints}: Theme) => createStyles({
+  root: {
+    display: 'flex'
+  },
+  toolbar: mixins.toolbar,
+  content: {
+    flexGrowt: 1,
+    [breakpoints.up('sm')]: {
+
+    }
+  }
+});
+
+interface AppProps {
+  classes: any;
+}
+
+const App: React.FC<AppProps> = props => {
+  
+  const { classes } = props;
+  
+  return (
+    <div className={classes.root}>
+      <MuiThemeProvider theme={applicationTheme}>
+        <CssBaseline />
+        <ApplicationWrapper>
+          Hello World
+        </ApplicationWrapper>
+      </MuiThemeProvider>
+    </div>
+  );
+
+}
+
+export default withStyles(styles)(App);
