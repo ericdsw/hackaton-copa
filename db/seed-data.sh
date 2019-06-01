126b46fece39f27fd2cad5455dc277613c34ad50
Christopher Francisco <christopher.f.almanza@gmail.com>
2019-06-01 14:20:09 -0500

    Seed automatically with 1 command

--- db/seed-data.sh
+++ db/seed-data.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-# mongoimport --db hc --collection flights --file /app/data/flights.json
+docker exec hc-db sh -c "mongoimport --db hc --collection flights --file /app/data/flights.json --jsonArray"
