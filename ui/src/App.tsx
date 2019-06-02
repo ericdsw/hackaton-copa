497f5002f0a96ae4299fae8f0a3f15c248c38149
Christopher Francisco <christopher.f.almanza@gmail.com>
2019-06-02 07:01:29 -0500

    Update visuals

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,13 +3,13 @@ import './App.css';
 import ApplicationWrapper from './components/commons/ApplicationWrapper';
 import { MuiThemeProvider, createMuiTheme, Theme } from '@material-ui/core/styles';
 import { CssBaseline, withStyles, createStyles } from '@material-ui/core';
-import { purple, green } from '@material-ui/core/colors';
+import { indigo, green } from '@material-ui/core/colors';
 
 import WontShow from './components/pages/WontShow';
 
 const applicationTheme = createMuiTheme({
   palette: {
-    primary: purple,
+    primary: indigo,
     secondary: green
   }
 });
